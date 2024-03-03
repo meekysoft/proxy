408636262870735cb164950417d435301e70615d
Peter Hehn <pete@Peters-MacBook-Pro.local>
2024-03-03 18:53:44 +0000

    tidy

--- modules/ec2-proxy/main.tf
+++ modules/ec2-proxy/main.tf
@@ -38,10 +38,7 @@ resource "aws_security_group" "security_group" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    # ipv6_cidr_blocks = ["::/0"]
   }
-
-  #   86.134.162.101
 
   ingress {
     from_port   = 22
